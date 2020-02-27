82f34c97c0c9af1838b204e59e46ab0718af723a
SarahHouben <41924354+SarahHouben@users.noreply.github.com>
2020-02-27 16:52:34 +0100

    Create index.js

---
+++ index.js
@@ -0,0 +1,8 @@
+module.exports = {
+  configs: {
+    base: require("./src/base"),
+    node: require("./src/node"),
+    react: require("./src/react"),
+    typescript: require("./src/typescript")
+  }
+};
