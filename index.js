caa711e0b1dc91d1262ac51af13fda41a61a8ed9
SarahHouben <41924354+SarahHouben@users.noreply.github.com>
2020-02-27 17:25:28 +0100

    Delete index.js

--- index.js
+++
@@ -1,8 +0,0 @@
-module.exports = {
-  configs: {
-    base: require("./src/base"),
-    node: require("./src/node"),
-    react: require("./src/react"),
-    typescript: require("./src/typescript")
-  }
-};
