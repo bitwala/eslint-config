1d868aebf191cf6ce4990854921fe085014a1751
SarahHouben <41924354+SarahHouben@users.noreply.github.com>
2020-02-27 15:12:25 +0100

    Update no-console

--- src/base.js
+++ src/base.js
@@ -31,7 +31,7 @@ module.exports = {
     "import/order": "error",
     "import/named": "off",
     "import/no-unresolved": "off",
-    "no-console": "off",
+    "no-console": "error",
     "no-restricted-globals": "error",
     "no-unused-expressions": "off",
     "no-useless-constructor": "error",
