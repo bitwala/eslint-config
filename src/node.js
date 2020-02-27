4d59dc126ba1b4e16a92331df3b3e731531ab9f4
SarahHouben <41924354+SarahHouben@users.noreply.github.com>
2020-02-27 17:12:01 +0100

    Update node.js

--- src/node.js
+++ src/node.js
@@ -2,6 +2,9 @@ module.exports = {
   env: {
     node: true
   },
+  parserOptions: {
+    ecmaVersion: 2020
+  },
   extends: ["./base.js", "plugin:node/recommended"],
   plugins: ["eslint-plugin-node"]
 };
