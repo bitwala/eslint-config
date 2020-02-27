cafb2c5e1d3d58218dfd2bee33e8d0ab89518f58
SarahHouben <41924354+SarahHouben@users.noreply.github.com>
2020-02-27 15:13:36 +0100

    Update no-unused-vars

--- src/base.js
+++ src/base.js
@@ -23,7 +23,7 @@ module.exports = {
     "@typescript-eslint/no-non-null-assertion": "warn",
     "@typescript-eslint/no-use-before-define": "error",
     "@typescript-eslint/no-explicit-any": "warn",
-    "@typescript-eslint/no-unused-vars": "warn",
+    "@typescript-eslint/no-unused-vars": "error",
     "@typescript-eslint/camelcase": ["error", { properties: "never" }],
     "implicit-dependencies/no-implicit": "error",
     camelcase: "off",
