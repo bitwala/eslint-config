baace44a9aa49ee3dca66725816f23adab707b86
SarahHouben <41924354+SarahHouben@users.noreply.github.com>
2020-03-02 11:57:25 +0100

    chore: update config files (#4)

--- node.js
+++ node.js
@@ -2,5 +2,15 @@ module.exports = {
   env: {
     node: true
   },
-  extends: ["./base.js"]
+  parserOptions: {
+    ecmaVersion: 2018
+  },
+  extends: ["./base.js", "plugin:node/recommended"],
+  plugins: ["eslint-plugin-node"],
+  rules: {
+    "node/no-extraneous-import": "error",
+    "node/no-missing-import": "off",
+    "node/no-unpublished-import": "off",
+    "node/no-unsupported-features/es-syntax": "off"
+  }
 };
--- node.production.js
+++
@@ -1,7 +0,0 @@
-module.exports = {
-  extends: ["./base.js", "./node.js"],
-  rules: {
-    "no-console": "error",
-    "@typescript-eslint/no-unused-vars": "error"
-  }
-};
