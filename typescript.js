070d407506600e005d0565064c20d3ed0e5c1d2f
SarahHouben <41924354+SarahHouben@users.noreply.github.com>
2020-05-20 13:36:44 +0200

    chore: update rules (#8)

--- typescript.js
+++ typescript.js
@@ -2,36 +2,41 @@ module.exports = {
   extends: [
     "./base.js",
     "plugin:@typescript-eslint/eslint-recommended",
-    "plugin:@typescript-eslint/recommended"
+    "plugin:@typescript-eslint/recommended",
   ],
-  parser: "@typescript-eslint/parser",
-  parserOptions: {
-    ecmaFeatures: {
-      jsx: true
+  overrides: [
+    {
+      parser: "@typescript-eslint/parser",
+      parserOptions: {
+        ecmaFeatures: {
+          jsx: true,
+        },
+        ecmaVersion: 2018,
+        sourceType: "module",
+      },
+      files: ["**/*.ts", "**/*.tsx"],
+      plugins: ["@typescript-eslint"],
+      rules: {
+        "@typescript-eslint/interface-name-prefix": "off",
+        "@typescript-eslint/explicit-function-return-type": "off",
+        "@typescript-eslint/no-namespace": "off",
+        "@typescript-eslint/no-unused-expressions": "error",
+        "@typescript-eslint/no-untyped-public-signature": "error",
+        "@typescript-eslint/no-inferrable-types": "off",
+        "@typescript-eslint/no-non-null-assertion": "warn",
+        "@typescript-eslint/no-use-before-define": [
+          "warn",
+          {
+            functions: false,
+            classes: false,
+            variables: false,
+            typedefs: false,
+          },
+        ],
+        "@typescript-eslint/no-explicit-any": "warn",
+        "@typescript-eslint/no-unused-vars": "error",
+        "@typescript-eslint/camelcase": ["error", { properties: "never" }],
+      },
     },
-    ecmaVersion: 2018,
-    sourceType: "module"
-  },
-  plugins: ["@typescript-eslint"],
-  rules: {
-    "@typescript-eslint/interface-name-prefix": "off",
-    "@typescript-eslint/explicit-function-return-type": "off",
-    "@typescript-eslint/no-namespace": "off",
-    "@typescript-eslint/no-unused-expressions": "error",
-    "@typescript-eslint/no-untyped-public-signature": "error",
-    "@typescript-eslint/no-inferrable-types": "off",
-    "@typescript-eslint/no-non-null-assertion": "warn",
-    "@typescript-eslint/no-use-before-define": [
-      "warn",
-      {
-        functions: false,
-        classes: false,
-        variables: false,
-        typedefs: false
-      }
-    ],
-    "@typescript-eslint/no-explicit-any": "warn",
-    "@typescript-eslint/no-unused-vars": "error",
-    "@typescript-eslint/camelcase": ["error", { properties: "never" }]
-  }
+  ],
 };
