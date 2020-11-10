886f9771606d83f9169423d0f9c4855810c56c04
Léo Pradel <pradel.leo@gmail.com>
2020-11-10 12:38:34 +0100

    feat: detect eslint config automatically (#13)

---
+++ auto.js
@@ -0,0 +1,30 @@
+/* eslint-disable @typescript-eslint/no-var-requires, import/order */
+const path = require("path");
+
+const packageJsonPath = path.join(process.cwd(), "package.json");
+const packageJson = require(packageJsonPath);
+
+const hasReact = Boolean(
+  (packageJson.dependencies && packageJson.dependencies["react"]) ||
+    (packageJson.devDependencies && packageJson.devDependencies["react"])
+);
+
+const hasTypescript = Boolean(
+  packageJson.devDependencies && packageJson.devDependencies["typescript"]
+);
+
+module.exports = {
+  extends: [
+    "@bitwala/eslint-config/base",
+    // If project is using react we load the react config
+    hasReact ? "@bitwala/eslint-config/react" : null,
+    // If project is not using react it means it's a node project
+    !hasReact ? "@bitwala/eslint-config/node" : null,
+    // If project is using typescript we load the typescript config
+    hasTypescript ? "@bitwala/eslint-config/typescript" : null,
+  ].filter((s) => !!s),
+  parserOptions: {
+    ecmaVersion: 2018,
+    sourceType: "module",
+  },
+};
